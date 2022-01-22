fb194547c24cd1b7bd89b17ecc4e937f89bb653a
Gerald Annan <annang2@stanbic.com.gh>
2022-01-22 23:19:26 +0000

    Updated remote car struct

--- lib/remote_control_car.ex
+++ lib/remote_control_car.ex
@@ -1,20 +1,20 @@
 defmodule RemoteControlCar do
-  defstruct battery_percentage: 100, distance_driven_in_meters: 0, nickname: "none"
-
+  defstruct battery_percentage: 100, distance_driven_in_meters: 0, nickname: nil
+  @type remote_control_car :: %RemoteControlCar{}
   def new() do
-    %RemoteControlCar{}
+    Map.update!(%RemoteControlCar{}, :nickname, fn _ -> "none" end)
   end
 
   def new(nickname) do
     %{new() | nickname: nickname}
   end
 
-  @spec display_distance(map :: %RemoteControlCar{}) :: <<_::56, _::_*8>>
+  @spec display_distance(remote_control_car()) :: <<_::56, _::_*8>>
   def display_distance(remote_car) do
     "#{Map.fetch!(remote_car, :distance_driven_in_meters)} meters"
   end
 
-  @spec display_battery(map :: %RemoteControlCar{}) :: <<_::64, _::_*8>>
+  @spec display_battery(map :: remote_control_car()) :: <<_::64, _::_*8>>
   def display_battery(remote_car) do
     case Map.fetch!(remote_car, :battery_percentage) > 0 do
       true -> "Battery at #{Map.fetch!(remote_car, :battery_percentage)}%"
@@ -22,7 +22,7 @@ defmodule RemoteControlCar do
     end
   end
 
-  @spec drive(map :: %RemoteControlCar{}) :: map :: %RemoteControlCar{}
+  @spec drive(map :: remote_control_car()) :: map :: %RemoteControlCar{}
   def drive(remote_car) do
     case Map.fetch!(remote_car, :battery_percentage) > 0 do
       true ->
